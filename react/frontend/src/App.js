99e81beab0a3fe001117a1c69d632ed32fa97a7b
OliwiaTheUnicorn <oliwia.katarina.rozek@gmail.com>
2021-03-01 21:22:47 +0100

    updated frontend
    Signed-off-by: OliwiaTheUnicorn <oliwia.katarina.rozek@gmail.com>

--- react/frontend/src/App.js
+++ react/frontend/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
+import{BrowserRouter} from 'react-router-dom';
+import MainRouter from "./MainRouter"
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>React Frontend</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <MainRouter />
+  </BrowserRouter>
+)
 
 export default App;
 
